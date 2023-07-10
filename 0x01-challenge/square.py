4c66e7f5b95cd293fb8259d3037a8139f8bbce6d
Owolabi Pius <opius2007@gmail.com>
2023-07-10 21:19:30 +0100

    Fix my code update

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -4,7 +4,7 @@ class Square():
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
-    
+
     def area_of_my_square(self):
         """ Area of the square """
         return self.width * self.height
