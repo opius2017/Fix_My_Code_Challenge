62efaa20f0b80f10c28b5d44938bf6e2c822cffc
Owolabi Pius <opius2007@gmail.com>
2023-07-10 21:25:47 +0100

    Fix my code update

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -10,10 +10,11 @@ class Square:
         return self.width * self.height
 
     def perimeter_of_my_square(self):
-        return (self.width * 2) + (self.height * 2)
+        return 2 * (self.width + self.height)
 
     def __str__(self):
-        return "{}/{}".format(self.width, self.height)
+        return f"Square(width={self.width}, height={self.height})"
+
 
 if __name__ == "__main__":
     s = Square(width=12, height=9)
