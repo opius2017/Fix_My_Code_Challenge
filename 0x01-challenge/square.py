e7a12a6c950bc4e925b998e8766d969e851f8457
Owolabi Pius <opius2007@gmail.com>
2023-07-14 17:03:09 +0100

    Square update

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,20 +1,51 @@
 #!/usr/bin/python3
 
 class Square:
+    """
+    A class representing a square shape.
+    
+    Attributes:
+    - width: The width of the square.
+    - height: The height of the square.
+    """
+
     def __init__(self, width=0, height=0):
+        """
+        Initializes a square with the given width and height.
+        
+        Args:
+        - width: The width of the square.
+        - height: The height of the square.
+        """
         self.width = width
         self.height = height
 
     def area_of_my_square(self):
-        """ Area of the square """
+        """
+        Calculates the area of the square.
+        
+        Returns:
+        The area of the square.
+        """
         return self.width * self.height
 
     def perimeter_of_my_square(self):
-        return 2 * (self.width + self.height)
+        """
+        Calculates the perimeter of the square.
+        
+        Returns:
+        The perimeter of the square.
+        """
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        return f"Square(width={self.width}, height={self.height})"
-
+        """
+        Returns a string representation of the square.
+        
+        Returns:
+        A string representation of the square in the format 'width/height'.
+        """
+        return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
     s = Square(width=12, height=9)
